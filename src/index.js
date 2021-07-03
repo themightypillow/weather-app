27d626d74e784c6b29539988582cc935e2670f38
Cheri <mightypillow@tutanota.com>
2021-07-02 21:31:17 -0400

    Make weather icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import * as svg from "./svg";
 
 document.querySelector("main > div").appendChild(svg.sun.cloneNode(true));
+document.querySelector("main > div").appendChild(svg.cloud.cloneNode(true));
+document.querySelector("main > div").appendChild(svg.rain.cloneNode(true));
+document.querySelector("main > div").appendChild(svg.snow.cloneNode(true));
+document.querySelector("main > div").appendChild(svg.storm.cloneNode(true));
+document.querySelector("main > div").appendChild(svg.wind.cloneNode(true));
--- src/svg.js
+++ src/svg.js
@@ -1,37 +1,90 @@
-const main = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-main.setAttributeNS(null, "width", "48");
-main.setAttributeNS(null, "height", "48");
-main.setAttributeNS(null, "viewBox", "0 0 24 24");
-main.setAttributeNS(null, "stroke-width", "1");
-main.setAttributeNS(null, "stroke", "#c9c9c9");
-main.setAttributeNS(null, "fill", "none");
-main.setAttributeNS(null, "stroke-linecap", "round");
-main.setAttributeNS(null, "stroke-linejoin", "round");
+const base = (size, color, thickness) => {
+  const main = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  main.setAttributeNS(null, "width", size);
+  main.setAttributeNS(null, "height", size);
+  main.setAttributeNS(null, "viewBox", "0 0 24 24");
+  main.setAttributeNS(null, "stroke-width", thickness);
+  main.setAttributeNS(null, "stroke", color);
+  main.setAttributeNS(null, "fill", "none");
+  main.setAttributeNS(null, "stroke-linecap", "round");
+  main.setAttributeNS(null, "stroke-linejoin", "round");
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttributeNS(null, "stroke", "none");
+  path.setAttributeNS(null, "d", "M0 0h24v24H0z");
+  path.setAttributeNS(null, "fill", "none");
+  main.appendChild(path);
+  return main;
+};
 
-const base = document.createElementNS("http://www.w3.org/2000/svg", "path");
-base.setAttributeNS(null, "stroke", "none");
-base.setAttributeNS(null, "d", "M0 0h24v24H0z");
-base.setAttributeNS(null, "fill", "none");
-main.appendChild(base);
-
-const makeCircle = (data) => {
+const makeCircle = (data, fill = "none") => {
   const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   circle.setAttributeNS(null, "cx", data.cx);
   circle.setAttributeNS(null, "cy", data.cy);
   circle.setAttributeNS(null, "r", data.r);
+  circle.setAttributeNS(null, "fill", fill);
   return circle;
 }
 
-const makePath = (d) => {
+const makePath = (d, stroke = false, fill = false, width = false) => {
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
   path.setAttributeNS(null, "d", d);
+  if(stroke) path.setAttributeNS(null, "stroke", stroke);
+  if(fill) path.setAttributeNS(null, "fill", fill);
+  if(width) path.setAttributeNS(null, "stroke-width", width);
   return path;
 }
 
+const makePoly = (points, stroke = false, width = false) => {
+  const poly = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+  poly.setAttributeNS(null, "points", points);
+  if(stroke) poly.setAttributeNS(null, "stroke", stroke);
+  if(width) poly.setAttributeNS(null, "stroke-width", width);
+  return poly;
+}
+
 export const sun = (() => {
-  const svg = main.cloneNode(true);
-  svg.appendChild(makeCircle({cx: "12", cy: "12", r: "4"}));
+  const svg = base("150", "#f5cc62", "0.5");
+  svg.appendChild(makeCircle({cx: "12", cy: "12", r: "4"}, "#fdf0d0"));
   svg.appendChild(makePath("M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"));
   return svg;
 })();
 
+export const cloud = (() => {
+  const svg = base("150", "#c9c9c9", "0.5");
+  svg.appendChild(makePath("M0 0h24v24H0z", "none", "none"));
+  svg.appendChild(makePath("M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7h-12", false, "#ececec"));
+  return svg;
+})();
+
+export const rain = (() => {
+  const svg = base("150", "#c9c9c9", "0.5");
+  svg.appendChild(makePath("M0 0h24v24H0z", "none", "none"));
+  svg.appendChild(makePath("M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7", false, "#f4f4f4"));
+  svg.appendChild(makePath("M11 13v2m0 3v2m4 -5v2m0 3v2", "#00abfb"));
+  return svg;
+})();
+
+export const snow = (() => {
+  const svg = base("150", "#c9c9c9", "0.5");
+  svg.appendChild(makePath("M0 0h24v24H0z", "none", "none"));
+  svg.appendChild(makePath("M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7", false, "#f4f4f4"));
+  svg.appendChild(makePath("M11 15v.01m0 3v.01m0 3v.01m4 -4v.01m0 3v.01", "#b8d1de", false, "1"));
+  return svg;
+})();
+
+export const storm = (() => {
+  const svg = base("150", "#c9c9c9", "0.5");
+  svg.appendChild(makePath("M0 0h24v24H0z", "none", "none"));
+  svg.appendChild(makePath("M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7", false, "#f4f4f4"));
+  svg.appendChild(makePoly("13 14 11 18 14 18 12 22", "#ffd466", "1"));
+  return svg;
+})();
+
+export const wind = (() => {
+  const svg = base("150", "#b8d1de", "1");
+  svg.appendChild(makePath("M0 0h24v24H0z", "none", "none"));
+  svg.appendChild(makePath("M5 8h8.5a2.5 2.5 0 1 0 -2.34 -3.24"));
+  svg.appendChild(makePath("M3 12h15.5a2.5 2.5 0 1 1 -2.34 3.24"));
+  svg.appendChild(makePath("M4 16h5.5a2.5 2.5 0 1 1 -2.34 3.24"));
+  return svg;
+})();
