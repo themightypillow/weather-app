b7ab28777a997182a9e49cb675c4d96bb66f2737
Cheri <mightypillow@tutanota.com>
2021-07-06 02:52:26 -0400

    Display location weather on enter

--- src/ui.js
+++ src/ui.js
@@ -3,10 +3,24 @@ import * as svg from "./svg";
 
 (function() {
 
+  const isLoading = function(bool) {
+    document.querySelectorAll(".cover").forEach(cover => cover.style.display = bool ? "block" : "none");
+    document.querySelector(".lds-default").style.display = bool ? "inline-block" : "none";
+    document.querySelector("#search-box input").disabled = bool;
+    document.querySelector("#search-box").style.visibility = bool ? "hidden" : "visible";
+  };
+
+  const addIcon = function(parent, iconName) {
+    if(parent.querySelector("svg")) {
+      parent.replaceChild(svg[iconName].cloneNode(true), parent.querySelector("svg"));
+    }
+    else {
+      parent.appendChild(svg[iconName].cloneNode(true));
+    }
+  }
+
   const displayWeather = async function(location) {
-    // show loading
-    document.querySelectorAll(".cover").forEach(cover => cover.style.display = "block");
-    document.querySelector(".lds-default").style.display = "inline-block";
+    isLoading(true);
 
     const data = await weather.getData(location);
     console.log(data);
@@ -14,15 +28,15 @@ import * as svg from "./svg";
     // display top box
     document.querySelector("#big-temp").textContent = `${data.temp}°`;
     document.querySelector("#city").textContent = data.city;
-    document.querySelector("#big-icon").appendChild(svg[data.icon]);
-
+    addIcon(document.querySelector("#big-icon"), data.icon);
+  
     // display middle hourly box
     document.querySelectorAll("#middle-box > div > div").forEach((hourBox, index) => {
       const hourData = data.hourly[index];
       const [time, period] = hourData.time.split(" ");
 
       hourBox.querySelector(".bold.small").textContent = `${hourData.temp}°`;
-      hourBox.querySelector(".small-icon").appendChild(svg[hourData.icon].cloneNode(true));
+      addIcon(hourBox.querySelector(".small-icon"), hourData.icon)
       hourBox.querySelector(".bold.big").textContent = time;
       hourBox.querySelector(".period").textContent = period;
     });
@@ -32,18 +46,21 @@ import * as svg from "./svg";
       const dayData = data.weekly[index];
 
       dayBox.querySelector(".bold.small").textContent = `${dayData.temp}°`;
-      dayBox.querySelector(".small-icon").appendChild(svg[dayData.icon].cloneNode(true));
+      addIcon(dayBox.querySelector(".small-icon"), dayData.icon)
       dayBox.querySelector(".bold.big").textContent = dayData.time;
     });
 
-    // hide loading
-    document.querySelectorAll(".cover").forEach(cover => cover.style.display = "none");
-    document.querySelector(".lds-default").style.display = "none";
+    isLoading(false);
   };
   
   (async function() {
     // initialize weather screen with default
     await displayWeather("New York, NY");
+    document.querySelector("#search-box input").addEventListener("keypress", (e) => {
+      if(e.key === "Enter") {
+        displayWeather(e.target.value.trim());
+      }
+    });
   })();
 
   // add error overlay when can't load weather
